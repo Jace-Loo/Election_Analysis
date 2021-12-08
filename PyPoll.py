918323f8012962dd5c8574d4fda95d6ce4f1f497
Jaclyn Loo <loojaclyn@gmail.com>
2021-12-07 18:31:50 -0800

    updated election results code and included results summary in text file

--- PyPoll.py
+++ PyPoll.py
@@ -71,49 +71,75 @@ with open(file_to_load) as election_data:
         # add 1 to vote count
         candidate_votes[candidate_name] += 1
 
-# # print candidate dictionary with number of votes (no longer needed in final results)
-# print(candidate_votes) 
+    # save results to our text file
+    with open(file_to_save, "w") as txt_file:
+        # store content for text file in variables
+        election_results = (
+        # insert header in the text file
+            "\nElection Results\n"
+            "-----------------------\n"
+            f"Total Votes: {total_votes:,}\n" # ":," is a thousands separator! 
+            "-----------------------\n")
+
+        # print text file content
+        print(election_results, end="") # ending print with parameter end="" equal to am empty string so the last line is blank
+
+        # save final vote count to text file
+        txt_file.write(election_results)
     
-# calculate % of votes for each candidate
-# iterate through candidate list (for each variable in dictionary)
-for candidate_name in candidate_votes:
+        
+        # calculate % of votes for each candidate (and add to text file)
+        # iterate through candidate list (for each variable in dictionary)
+        for candidate_name in candidate_votes:
 
-    # get vote count for candidate (declare variable for number of votes: votes; dictionary_of_votes[value or number of votes])
-    votes = candidate_votes[candidate_name]
+            # get vote count for candidate (declare variable for number of votes: votes; dictionary_of_votes[value or number of votes])
+            votes = candidate_votes[candidate_name]
 
-    # calculate percentage of votes 
-    # declare vote_percentage variable, declare number of votes per candidate and number of total votes as floats)
-    vote_percentage = float(votes) / float(total_votes) * 100
-    # vote_percentage = round(vote_percentage, 1), skill drill to round to 1 decimal place, done later a diff way
+            # calculate percentage of votes 
+            # declare vote_percentage variable, declare number of votes per candidate and number of total votes as floats)
+            vote_percentage = float(votes) / float(total_votes) * 100
+            # vote_percentage = round(vote_percentage, 1), skill drill to round to 1 decimal place, done later a diff way
 
-    # print the candidate's name and their percentage of votes
-    # use print(f) to use code in a string (instead of it being literal) (":.1f" rounds float to 1 decimal place)
-    print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            # print the candidate's name and their percentage of votes
+            # use print(f) to use code in a string (instead of it being literal) (":.1f" rounds float to 1 decimal place)
+            # print(f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
 
-    # Determine winning candidate
-    # determine if the votes are greater than the winning count and percentage (start at 0) 
-    # this iterates through candidates and replaces the value of the winners each time it encounters someone with higher values
-    if (votes > winning_count) and (vote_percentage > winning_percentage):
-        # if they are greater, 
-        # make the new winning count and winning percentage equal to value of votes and vote_percentage for current candidate
-        # vote_percentage
-        winning_count = votes
-        winning_percentage = vote_percentage
-        # set current candidate to winning_candidate
-        winning_candidate = candidate_name
+            # store content for text file in variable
+            candidate_results = (
+                f"{candidate_name}: {vote_percentage:.1f}% ({votes:,})\n")
+            
+            # print each candidate, their voter count, and percentage to the terminal 
+            print(candidate_results)
 
-# print(winning_count, winning_percentage, winning_candidate) (just using to check winning candidate code)
+            # save candidate results to text file
+            txt_file.write(candidate_results)  
+ 
 
-# print out summary of winner
-# create a variable to hold info
-winning_candidate_summary = (
-    f"----------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n" # ":," means rounded to 0 decimal places??
-    f"Winning Percentage: {winning_percentage:.1f}%\n" # rounding the float to 1 decimal place??
-    f"----------\n"
-)
-print(winning_candidate_summary)
+            # Determine winning candidate
+            # determine if the votes are greater than the winning count and percentage (start at 0) 
+            # this iterates through candidates and replaces the value of the winners each time it encounters someone with higher values
+            if (votes > winning_count) and (vote_percentage > winning_percentage):
+                # if they are greater, 
+                # make the new winning count and winning percentage equal to value of votes and vote_percentage for current candidate
+                # vote_percentage
+                winning_count = votes
+                winning_percentage = vote_percentage
+                # set current candidate to winning_candidate
+                winning_candidate = candidate_name
+
+        # print(winning_count, winning_percentage, winning_candidate) (just using to check winning candidate code)
+
+        # print out summary of winner
+        # create a variable to hold info
+        winning_candidate_summary = (
+            f"----------\n"
+            f"Winner: {winning_candidate}\n"
+            f"Winning Vote Count: {winning_count:,}\n" # ":," means rounded to 0 decimal places??
+            f"Winning Percentage: {winning_percentage:.1f}%\n" # rounding the float to 1 decimal place??
+            f"----------\n"
+        )
+        # save winning candidate's summary to text file
+        txt_file.write(winning_candidate_summary)
 
 ###
 
